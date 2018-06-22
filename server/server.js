5c9f447a78360ba85b5e18aaa35599b2f82454c8
Mike Miner <mike.miner@verys.com>
2018-06-22 15:13:08 -0700

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -9,13 +10,43 @@ var app = express();
 
 app.use(bodyParser.json());
 
-// app.get('/todos/:id', (req, res) => {
-//     Todo.findOne({}).then((todos) => {
-//         res.send(todos);
-//     }).catch((err) => {
-//         res.status(400).send(err)
-//     })
-// });
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+    else {
+        Todo.findById(id).then((todo) => {
+            if(!todo) {
+                res.status(404).send();
+            }
+            else {
+                res.send({todo});
+            }
+        }).catch((err) => {
+            res.status(400).send();
+        })
+    }
+});
+
+app.put('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+    else {
+        Todo.findById(id).then((todo) => {
+            if(!todo) {
+                res.status(404).send();
+            }
+            else {
+                res.send({todo});
+            }
+        }).catch((err) => {
+            res.status(400).send();
+        })
+    }
+});
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
